c5cccefa8522c4421d21d41aba238d58d198b383
Adil <https://_redemption@bitbucket.org/_redemption/lu-canteen.git>
2018-05-25 16:07:24 +0600

    first

---
+++ app/src/main/java/com/teamflybd/oreobatch/MainActivity.java
@@ -0,0 +1,19 @@
+package com.teamflybd.oreobatch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+/*
+    Adil Ahmed Chowdhury
+    Github integration
+
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
