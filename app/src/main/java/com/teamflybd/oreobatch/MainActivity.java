09e60547334388f3599fa517d56a857768157d5b
Adil <https://_redemption@bitbucket.org/_redemption/lu-canteen.git>
2018-05-25 16:27:10 +0600

    first

--- app/src/main/java/com/teamflybd/oreobatch/MainActivity.java
+++ app/src/main/java/com/teamflybd/oreobatch/MainActivity.java
@@ -2,6 +2,8 @@ package com.teamflybd.oreobatch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 
 /*
@@ -15,5 +17,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void press(View view)
+    {
+        Toast.makeText(MainActivity.this,"Button has been pressed!!",Toast.LENGTH_LONG).show();
     }
 }
