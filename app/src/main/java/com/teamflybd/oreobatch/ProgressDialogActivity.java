34d6d5401d6e9a5aba69f5d0d4249d7a7df53919
Adil <https://_redemption@bitbucket.org/_redemption/lu-canteen.git>
2018-06-02 12:01:53 +0600

    ProgressDialog Added...

---
+++ app/src/main/java/com/teamflybd/oreobatch/ProgressDialogActivity.java
@@ -0,0 +1,33 @@
+package com.teamflybd.oreobatch;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ProgressDialogActivity extends AppCompatActivity {
+
+    Button button;
+    ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_progress_dialog);
+
+        button =(Button)findViewById(R.id.button);
+        progressDialog = new ProgressDialog(this);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressDialog.setTitle("Loading");
+                progressDialog.show();
+            }
+        });
+
+
+
+    }
+}
